7b2a418938f1976d44044bb4916594658ca959ce
cmitz <cmitzz@gmail.com>
2014-01-24 11:10:18 +0100

    Clienthandler.

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -1,5 +1,40 @@
-package Server;
+package rolit.server;
 
-public class ClientHandler {
+import java.io.*;
+import java.net.*;
+
+public class ClientHandler extends Thread {
+
+	private Server          server;
+	private Socket          sock;
+	private int				gameNumber = -1;
+	private BufferedReader  in;
+	private BufferedWriter  out;
+	private String          clientName = "[clientName]";
+
+	public ClientHandler(Server server, Socket sock) throws IOException {
+	}
+	
+	
+	public void run() {
+	}
+
+	public synchronized void sendMessage(String msg) {
+	}
+	
+	public synchronized void shutdown() {
+	}
+	
+	public String getClientName(){
+		return clientName;
+	}
+	
+	public void setGameNumber(int number){
+		gameNumber = number;
+	}
+	
+	public int getGameNumber(){
+		return gameNumber;
+	}
 
 }
