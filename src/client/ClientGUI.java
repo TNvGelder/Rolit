75ac9afa7a31d03cdb612ba4cc5a3225567cdcfd
TNvGelder <TNvGelder@gmail.com>
2014-01-24 13:18:44 +0100

    ClientGui

--- src/client/ClientGUI.java
+++ src/client/ClientGUI.java
@@ -1,55 +1,66 @@
 package client;
 import game.Game;
 
-import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 
-public class ClientGUI extends JFrame implements ActionListener{
+public class ClientGUI extends JFrame implements ActionListener, Observer{
+	private int DIM = 8;
 	private Container c = getContentPane();
-	private JButton[] buttonArray = new JButton[63];
+	private JPanel boardPanel = new JPanel();
+	private JButton[] buttonArray = new JButton[64];
 	private JLabel turnInfo = new JLabel("Playername's turn");
-	private int guiSize;
+	private int boardSize;
 	
 	public ClientGUI(int size){
-		guiSize = size;
+		super("Rolit Client");
+		boardSize = size;
 		init();
 	}
 
 	public void init(){
-		for (int i = 0; i < 63; i++){
+		for (int i = 0; i < 64; i++){
+			Border boardLine = BorderFactory.createLineBorder(Color.black);
+			
 			JButton fieldButton = new JButton();
-			fieldButton.setSize((guiSize - guiSize/10)/8,(guiSize - guiSize/10)/8);
 			buttonArray[i] = fieldButton;
-			c.add(buttonArray[i]);
-			c.setLayout(new FlowLayout());
+			boardPanel.add(buttonArray[i]);
+			boardPanel.setLayout(new GridLayout(DIM, DIM));
+			boardPanel.setBorder(boardLine);
+			boardPanel.setPreferredSize(new Dimension(boardSize,boardSize));
+			c.add(boardPanel);
 		}
-		c.add(turnInfo);
 		setLayout(new FlowLayout());
-		for (int i = 0; i < 10; i++){
-			c.add(buttonArray[i]);
-		}
+		setSize(boardSize*2,boardSize*2);
+		c.setMinimumSize(new Dimension(boardSize,boardSize));
 		c.add(turnInfo);
-		c.setSize(100,100);
-		setSize(guiSize,guiSize);
 		setVisible(true);
 	}	
 
 	
 	public static void main(String[] args){
-		ClientGUI test1 = new ClientGUI(800);
+		ClientGUI test1 = new ClientGUI(500);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
 		
 	}
