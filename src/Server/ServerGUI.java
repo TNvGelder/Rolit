4b4dac7fa09e1ea287a225e7f919626205397eea
cmitz <cmitzz@gmail.com>
2014-01-24 11:08:27 +0100

    Different set-up

--- src/Server/ServerGUI.java
+++ src/Server/ServerGUI.java
@@ -1,5 +1,12 @@
 package Server;
 
 public class ServerGUI {
-
+	private Server server;
+	public ServerGUI(Server server){
+		this.server = server;
+	}
+	public void update(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -4,71 +4,43 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Observable;
 import java.util.Set;
 
-public class Game {
+import Server.ClientHandler;
+
+public class Game extends Observable {
 	
-	Set<Player> waiters = new HashSet<Player>();
-	List<Player> players = new ArrayList<Player>();
-	Board gameBoard = new Board();
-	public Game(){
+	private int gameID;
+	private Set<ClientHandler> waiters = new HashSet<ClientHandler>();
+	private List<ClientHandler> players = new ArrayList<ClientHandler>();
+	private Board board;
+	private FieldType current;
+	
+	public Game(int id){
+		gameID = id;
 	}
 	
-	public void add(Player player){
+	public void add(ClientHandler player){
 		waiters.add(player);
 	}
-	public void doMove(int position, FieldType clr){
-		gameBoard.move(position, clr);
-	}
-	public void remove(Player player){
+	public void remove(ClientHandler player){
 		waiters.remove(player);
+	}
+	public void doMove(int position, FieldType color){
+		board.move(position, color);
 	}
 	
 	public Board getBoard(){
-		return gameBoard.copyBoard();
+		return board;
 	}
 	
+	/**
+	 * Start this game: create a board and keep things updated.
+	 */
 	public void startGame(){
-		if (waiters.size() < 2){
-			System.out.println("Not enough players");
-			return;
-		}
-		Iterator<Player> wait = waiters.iterator();
-		for (int i = 0; i < waiters.size(); i++){
-			Player nxtPlay = wait.next();
-			players.add(nxtPlay);
-			nxtPlay.setGame(this);
-			System.out.println(nxtPlay.getName() + " joined.");
-		}
-		if (players.size() == 2){
-			players.get(0).setColour(FieldType.RED);
-			players.get(1).setColour(FieldType.GREEN);
-		}
-		if (players.size() >= 3){
-			players.get(0).setColour(FieldType.RED);
-			players.get(1).setColour(FieldType.YELLOW);
-			players.get(2).setColour(FieldType.GREEN);
-		}
-		if (players.size() == 4){
-			players.get(3).setColour(FieldType.BLUE);
-		}
-		int i = 0;
-		while (i < players.size()){
-			doMove(players.get(i).giveMove(),players.get(i).getColor());
-			System.out.println(gameBoard.toString());
-			if (gameBoard.isFull()){
-				System.out.println("Red: " + gameBoard.getClrCount(FieldType.RED));
-				System.out.println("Yellow: " + gameBoard.getClrCount(FieldType.YELLOW));
-				System.out.println("Green: " + gameBoard.getClrCount(FieldType.GREEN));
-				System.out.println("Blue: " + gameBoard.getClrCount(FieldType.BLUE));
-				i = 9001;
-			}
-			if (i==players.size() -1){
-				i = 0;
-			}
-			else{
-				i++;
-			}
+		if (waiters.size()<2) {
+			
 		}
 		
 	}
